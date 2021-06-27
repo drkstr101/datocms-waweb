c96ce01d3b5e8eb5e56943861ccf04fef0e5fe3e
Aaron Miller <amiller@watheia.org>
2021-06-27 21:42:09 +0000

    upgrade deps

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   env: {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
       process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
